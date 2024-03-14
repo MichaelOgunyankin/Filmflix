9f87526f765f27d44558d87fa81e3da1f6f6354f
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-03-14 16:06:46 +0000

    Update record Now able to update film records

--- updaterecord.py
+++ updaterecord.py
@@ -9,41 +9,43 @@ def update_record():
     try:
         dbCon, dbCursor = db_access()
 
-        # check if the SongID exists
-        song_id = int(input("Enter SongID to update a record: "))
-        dbCursor.execute("SELECT * FROM songs WHERE SongID = ?",(song_id,))
+        # check if the FilmID exists
+        film_id = int(input("Enter FilmID to update a record: "))
+        dbCursor.execute("SELECT * FROM tblFilms WHERE filmID = ?",(film_id,))
 
         row = dbCursor.fetchone()
 
-        if row == None:# if there is no match with the song_id provided
-            print(f"No recod with SongID {song_id} exists in the table songs")
+        if row == None:# if there is no match with the film_id provided
+            print(f"No recod with FilmID {film_id} exists in the table films")
 
-        else:# if there is a match with the song_id provided
+        else:# if there is a match with the film_id provided
             num_fields = input("Enter N to update one field or Y to update all fields: ").upper()
 
             if num_fields == "Y":
                 # update all fields  
-                song_title  = input("Enter value to update song title: ")
-                song_artist = input("Enter value to update song artist: ")
-                song_genre  = input("Enter value to update song genre: ")
+                film_title = input("Enter film title: ")
+                year_released = int(input("Enter release year: "))
+                film_rating = input("Enter rating: ")
+                film_duration = int(input("Enter duration: "))
+                film_genre = input("Enter genre: ") 
 
                 # perform update 
-                dbCursor.execute("UPDATE songs SET Title =?, Artist=?, Genre=? WHERE SongID =?",(song_title,song_artist,song_genre, song_id))
+                dbCursor.execute("UPDATE tblFilms SET title =?, yearReleased=?, rating=?, duration=?, genre=? WHERE SongID =?",(film_title,year_released,film_rating, film_duration, film_genre,film_id))
                 dbCon.commit()
-                print(f"All fields in the record {song_id} updated in the songs table")
+                print(f"All fields in the record {film_id} updated in the films table")
 
             elif num_fields == "N":
                 #ask for the field to be updated 
-                field_name = input("Enter the field (Title or Artist or Genre): ").title()
-                if field_name not in ["Title", "Artist", "Genre"]:
+                field_name = input("Enter the field (Title or Release Year or  Rating or Duration or Genre): ").title()
+                if field_name not in ["Title", "Release Year", "Rating", "Duration", "Genre"]:
                     print(f"Field {field_name} not a valid field name in the table")
                 else:
                     #ask for the field value
                     field_value = input(f"Enter the value for the field {field_name}: ")
                     # perform update  on a specific field
-                    dbCursor.execute(f"UPDATE songs SET {field_name} =? WHERE SongID =?", (field_value, song_id,))
+                    dbCursor.execute(f"UPDATE tblFilms SET {field_name} =? WHERE filmID =?", (field_value, film_id,))
                     dbCon.commit()
-                    print(f"Record {song_id} updated in the songs table")
+                    print(f"Record {film_id} updated in the films table")
             else:
                 print("Invalid choice, please enter Y or N")
     except sql.OperationalError as e:
